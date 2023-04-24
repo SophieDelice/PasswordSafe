bb012cc0226e31c17c46c721ccc8661ef1e31406
Sophie Delice <delicesophie824@gmail.com>
2023-04-24 18:44:27 -0400

    wip:adds filesystem

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+# dummy password data for prototyping 
+class Password:
+   def __init__(self, services, useranme, password ):
+       self.services = services
+       self.username = useranme
+       self.password = password
+
+passwords = [
+    Password('Netflix', 777-999-4560, 'Level#leeyah' ),
+    Password('Hulu', 'testingemail@gmail.com', 'Rememberthis#nf'),
+    Password('Disney+', 'testingemail@gmail.com', 'You-bishvl'),
+]
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def passwords_index(request): 
+    return render(request, 'passwords/index.html', {'passwords': passwords})
