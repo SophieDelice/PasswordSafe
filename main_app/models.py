71a038587823ed386916f1d11fd1c0a7e8c4c8eb
Sophie Delice <delicesophie824@gmail.com>
2023-04-25 17:26:44 -0400

    wip: adds filesystem

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Password(models.Model):
@@ -8,3 +9,9 @@ class Password(models.Model):
 
 def __str__(self):
     return self.StreamingService
+
+# def get_absolute_urp(self):
+#     return reverse('passwords_detail', kwargs={'password_id': self.id})
+
+#    def get_absolute_url(self):
+#         return reverse ('books_detail', kwargs={'book_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from .models import Password
 
 # dummy password data for prototyping 
-class Password:
-   def __init__(self, services, useranme, password ):
-       self.services = services
-       self.username = useranme
-       self.password = password
+# class Password:
+#    def __init__(self, services, useranme, password ):
+#        self.services = services
+#        self.username = useranme
+#        self.password = password
 
-passwords = [
-    Password('Netflix', 'testtingemail@gmail.com', 'Level#leeyah' ),
-    Password('Hulu', 'testingemail@gmail.com', 'Rememberthis#nf'),
-    Password('Disney+', 'testingemail@gmail.com', 'You-bishvl'),
-]
+# passwords = [
+#     Password('Netflix', 'testtingemail@gmail.com', 'Level#leeyah' ),
+#     Password('Hulu', 'testingemail@gmail.com', 'Rememberthis#nf'),
+#     Password('Disney+', 'testingemail@gmail.com', 'You-bishvl'),
+# ]
 
 
 def home(request):
@@ -22,9 +22,9 @@ def about(request):
     return render(request,'about.html')
 
 def passwords_index(request): 
-    #passwords = Password.objects.all()
-    return render(request, 'passwords/index.html', {'passwords': passwords})
+    passwords = Password.objects.all()
+    return render(request, 'passwords/index.html', {'passwords':passwords})
 
 def passwords_detail(request, password_id): 
    password =  Password.objects.get(id=password_id) 
-   return render(request, 'passwords/detail.html', {'password':password})
+   return render(request, 'passwords/detail.html', {'password': password})
